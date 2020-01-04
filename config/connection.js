4a26ab67ed73fd568191f7f919815e700c74423b
ajreid <axr490@miami.edu>
2020-01-04 11:51:35 -0500

    added SQL functions to ORM.js

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,20 @@
 let mysql = require("mysql");
 
-module.exports = mysql;
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bootySHAKE",
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.log(`Connection Error: ${err.stack}`);
+        return
+    }
+
+    console.log(`Connected as ID: ${connection.threadID}`);
+});
+
+module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,31 @@
 let connection = require("./connection.js");
+
+const orm = {
+    selectAll: function(table) {
+        const queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function(table, columns, values) {
+        const queryString = `INSERT INTO ${table} (${columns.toString()}) VALUES (?)`;
+        console.log(queryString);
+        
+        connection.query(queryString, values, function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function(table, value, condition) {
+        const queryString = `UPDATE ${table} SET devoured = ? WHERE id = ?`;
+        console.log(queryString);
+
+        connection.query(queryString, [value, condition], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
+}
+
+module.exports = orm;
